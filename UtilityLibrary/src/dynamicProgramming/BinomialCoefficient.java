273b18bb2d45a93de3697e8621d0fc37bb628d47
mariocervera <mariocervera@gmail.com>
2016-05-25 21:03:33 +0200

    Improve format BinomialCoefficient.java

--- UtilityLibrary/src/dynamicProgramming/BinomialCoefficient.java
+++ UtilityLibrary/src/dynamicProgramming/BinomialCoefficient.java
@@ -1,12 +1,11 @@
 package dynamicProgramming;
 
 /**
- * This class provides a method to calculate binomial coefficients C(n,k).
- * Binomial coefficients count the number of ways to choose k elements out of n possibilities.
- * The method is based on dynamic programming. It applies a recurrence relation that is
- * based on Pascal's triangle; thus, we avoid the need to calculate the coefficients using the
- * formula n! / ((n-k)!k!) , which is inefficient and intermediate calculations may cause
- * arithmetic overflow (even though the final result usually fits within an integer).
+ * This class provides a method to calculate binomial coefficients C(n,k). Binomial coefficients
+ * count the number of ways to choose k elements out of n possibilities. The method is based on
+ * Dynamic Programming: it applies the recurrence relation that is defined by the Pascal's triangle;
+ * thus, we avoid the need to calculate the coefficients using the formula n!/((n-k)!*k!), which is
+ * inefficient and may cause arithmetic overflow.
  * 
  * 
  * @author Mario Cervera
@@ -14,34 +13,34 @@ package dynamicProgramming;
  */
 public class BinomialCoefficient {
 
-	private static final int MAX_VALUE = 100; //Change this value if necessary
-	
+	private static final int MAX_VALUE = 100; // Change this value if necessary
+
 	private static long[][] table = null;
-	
+
 	/*
 	 * This method initializes the table that stores the binomial coefficients
 	 */
 	private static void init() {
-		
+
 		table = new long[MAX_VALUE][MAX_VALUE];
-		
-        for (int i = 0; i < MAX_VALUE; i++) { 	
-        	table[i][0] = table[i][i] = 1;
-        	for (int j = 1; j < i; j++) {
-            	table[i][j] = (table[i-1][j-1] + table[i-1][j]);
-            }
-        }
+
+		for (int i = 0; i < MAX_VALUE; i++) {
+			table[i][0] = table[i][i] = 1;
+			for (int j = 1; j < i; j++) {
+				table[i][j] = (table[i - 1][j - 1] + table[i - 1][j]);
+			}
+		}
 	}
-	
+
 	/*
 	 * Returns C(n,k)
 	 */
 	public static long calculate(int n, int k) {
-		
-		if(table == null) init();
-		
-		if(n > MAX_VALUE || k > MAX_VALUE) return -1L;
-		
+
+		if (table == null) init();
+
+		if (n > MAX_VALUE || k > MAX_VALUE) return -1L;
+
 		return table[n][k];
 	}
 }
