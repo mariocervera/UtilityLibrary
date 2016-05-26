a529c2eb1355d3c9dff09714cd944f9be23b03d5
mariocervera <mariocervera@gmail.com>
2016-05-26 23:49:37 +0200

    Improved format of the classes EdgeNode and Graph

--- UtilityLibrary/src/graphs/EdgeNode.java
+++ UtilityLibrary/src/graphs/EdgeNode.java
@@ -1,9 +1,8 @@
 package graphs;
 
 /**
- * A list node implementation that enables the representation of weighted graphs by means of
- * adjacency lists. An edge (x,y) of a graph will be represented by an EdgeNode y in x's adjacency
- * list.
+ * A node implementation that enables the representation of weighted graphs by means of adjacency
+ * lists. An edge (x,y) of a graph will be represented by an EdgeNode y in x's adjacency list.
  * 
  * @author Mario Cervera
  *
@@ -11,48 +10,48 @@ package graphs;
 public class EdgeNode {
 
 	// Attributes
-	
+
 	private int y;
 	private int weight;
-    private EdgeNode next;
-    
-    // Constructors
-    
-    public EdgeNode(int y) {
-        this.y = y;
-        this.weight = 1;
-        this.next = null;
-    }
-    
-    // Getters and setters
-    
-    public EdgeNode(int y, int weight) {
-    	this.y = y;
-    	this.weight = weight;
-    	this.next = null;
-    }
-    
-    public int getY() {
-        return y;
-    }
-    
-    public void setY(int y) {
+	private EdgeNode next;
+
+	// Constructors
+
+	public EdgeNode(int y) {
+		this.y = y;
+		this.weight = 1;
+		this.next = null;
+	}
+
+	public EdgeNode(int y, int weight) {
+		this.y = y;
+		this.weight = weight;
+		this.next = null;
+	}
+	
+	// Getters and setters
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
 		this.y = y;
 	}
-    
-    public int getWeight() {
+
+	public int getWeight() {
 		return weight;
 	}
-    
-    public void setWeight(int weight) {
+
+	public void setWeight(int weight) {
 		this.weight = weight;
 	}
-    
-    public EdgeNode getNext() {
-        return next;
-    }
-    
-    public void setNext(EdgeNode next) {
-        this.next = next;
-    }
+
+	public EdgeNode getNext() {
+		return next;
+	}
+
+	public void setNext(EdgeNode next) {
+		this.next = next;
+	}
 }
--- UtilityLibrary/src/graphs/Graph.java
+++ UtilityLibrary/src/graphs/Graph.java
@@ -11,58 +11,56 @@ public class Graph {
 	private int numVertices; // Number of vertices of the graph
 	private EdgeNode[] edges; // Adjacency lists
 	private int[] degree; // The degree of each vertex
-	
+
 	/*
 	 * Constructor
 	 */
 	public Graph(int numVertices) {
-		
+
 		this.numVertices = numVertices;
-		this.edges = new EdgeNode[numVertices+1]; // Vertices are numbered from 1 to N
-		this.degree = new int[numVertices+1];
-		
-		for(int i = 0; i <= numVertices; i++) {
+		this.edges = new EdgeNode[numVertices + 1]; // Vertices are numbered from 1 to N
+		this.degree = new int[numVertices + 1];
+
+		for (int i = 0; i <= numVertices; i++) {
 			this.edges[i] = null;
 			this.degree[i] = 0;
 		}
 	}
-	
+
 	/*
-	 * This method adds a new edge to the graph (weighted).
-	 * Returns true if the insertion is successful; false otherwise
+	 * This method adds a new edge to the graph (weighted). Returns true if the
+	 * insertion is successful; false otherwise
 	 */
 	public boolean insertEdge(int x, int y, int weight, boolean undirected) {
-        
-		if(x < 1 || x > numVertices 
-				|| y < 1 || y > numVertices) return false;
-		
-        if(edges[x] == null) {
-        	edges[x] = new EdgeNode(y, weight);
-        }
-        else {
-            EdgeNode newNode = new EdgeNode(y, weight);
-            newNode.setNext(edges[x]);
-            edges[x] = newNode;
-        }
-        
-        degree[x]++;
-        
-        if(undirected) {
-        	
-        	//If the graph is undirected, insert also the edge (y,x)
-        	
-            return insertEdge(y, x, weight, false);
-        }
-        
-        return true;
-    }
-	
+
+		if (x < 1 || x > numVertices || y < 1 || y > numVertices)
+			return false;
+
+		if (edges[x] == null) {
+			edges[x] = new EdgeNode(y, weight);
+		}
+		else {
+			EdgeNode newNode = new EdgeNode(y, weight);
+			newNode.setNext(edges[x]);
+			edges[x] = newNode;
+		}
+
+		degree[x]++;
+
+		if (undirected) { // If the graph is undirected, insert also the edge (y,x)
+
+			return insertEdge(y, x, weight, false);
+		}
+
+		return true;
+	}
+
 	/*
 	 * This method adds a new edge to the graph (unweighted --> weight = 1)
 	 */
 	public boolean insertEdge(int x, int y, boolean undirected) {
-		
+
 		return this.insertEdge(x, y, 1, undirected);
 	}
-	
+
 }
