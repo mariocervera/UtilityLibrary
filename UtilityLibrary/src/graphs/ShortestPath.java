53b8e0e6591ac557aeca92a803613575652624df
mariocervera <mariocervera@gmail.com>
2016-08-11 14:47:41 +0200

    Improved format of ShortestPath class

--- UtilityLibrary/src/graphs/ShortestPath.java
+++ UtilityLibrary/src/graphs/ShortestPath.java
@@ -18,63 +18,63 @@ public class ShortestPath {
 	 * from the starting vertex to every other vertex in the graph.
 	 */
 	public static long[] dijkstra(Graph graph, int start) {
-		
+
 		// Initialize variables
-		
+
 		int n = graph.getNumVertices();
 		EdgeNode[] edges = graph.getEdges();
-		
+
 		long[] distance = new long[n + 1];
 		boolean[] inTree = new boolean[n + 1];
-		
+
 		for(int i = 0; i <= n; i++) {
 			distance[i] = Long.MAX_VALUE;
 			inTree[i] = false;
 		}
-		
+
 		// The distance to the starting vertex from the starting vertex is 0
-		
+
 		distance[start] = 0;
-		
+
 		// While there are vertices outside of the shortest paths tree ...
-		
+
 		int node = start;
-        
-        while(!inTree[node]) {
-            
-            inTree[node] = true;
-            
-            // Iterate outgoing edges of current vertex to update distances
-            
-            EdgeNode adjacentNode = edges[node];
-            
-            while(adjacentNode != null) {
-                
-                long w = adjacentNode.getWeight();
-                long currentDistance = distance[adjacentNode.getY()];
-                long newDistance = distance[node] + w;
-                
-                if(currentDistance > newDistance) {
-                    distance[adjacentNode.getY()] = newDistance; 
-                }
-                
-                adjacentNode = adjacentNode.getNext();
-            }
-            
-            // Select minimum distance
-            
-            node = start;
-            
-            long minDist = Long.MAX_VALUE;
-            
-            for(int i = 1; i <= n; i++) {
-               if(!inTree[i] && distance[i] < minDist) {
-                   minDist = distance[i];
-                   node = i;
-               }
-            }
-        }
-		
+
+		while(!inTree[node]) {
+
+			inTree[node] = true;
+
+			// Iterate outgoing edges of current vertex to update distances
+
+			EdgeNode adjacentNode = edges[node];
+
+			while(adjacentNode != null) {
+
+				long w = adjacentNode.getWeight();
+				long currentDistance = distance[adjacentNode.getY()];
+				long newDistance = distance[node] + w;
+
+				if(currentDistance > newDistance) {
+					distance[adjacentNode.getY()] = newDistance;
+				}
+
+				adjacentNode = adjacentNode.getNext();
+			}
+
+			// Select minimum distance
+
+			node = start;
+
+			long minDist = Long.MAX_VALUE;
+
+			for(int i = 1; i <= n; i++) {
+				if(!inTree[i] && distance[i] < minDist) {
+					minDist = distance[i];
+					node = i;
+				}
+			}
+		}
+
 		return distance;
 	}
 }
